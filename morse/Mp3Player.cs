655ab7d0833cf19f4f25cd4ed733dc78b7d79c99
孟熙武 <mengxiwu@hilifecd.com>
2025-02-24 15:59:19 +0800

    refactor:调整目录结构

--- morse/Mp3Player.cs
+++ morse/Mp3Player.cs
@@ -9,28 +9,35 @@ namespace CW
 {
     internal class Mp3Player
     {
-        private static WaveOut? waveOut ;
-        private static Mp3FileReader? mp3 ;
-        public static void Play(string path) {
+        private static WaveOut? waveOut;
+        private static Mp3FileReader? mp3;
+        public static void Play(string path)
+        {
             //文件不存在，不播放
-            if (!File.Exists(path)) { 
-            return;
+            if (!File.Exists(path))
+            {
+                return;
             }
             Stop();
-           mp3 =new Mp3FileReader(path);
+            mp3 = new Mp3FileReader(path);
             waveOut = new WaveOut();
             waveOut.Init(mp3);
             waveOut.Play();
-            
+
         }
-        public static void Pause() {
-            if (waveOut != null&&waveOut.PlaybackState== PlaybackState.Playing) {
+        public static void Pause()
+        {
+            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
+            {
                 waveOut.Pause();
-            } 
-        }        public static void ContinuePlay() {
-            if (waveOut != null&&waveOut.PlaybackState== PlaybackState.Paused) {
+            }
+        }
+        public static void ContinuePlay()
+        {
+            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Paused)
+            {
                 waveOut.Play();
-            } 
+            }
         }
 
         public static void Stop()
@@ -38,7 +45,7 @@ namespace CW
             if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
             {
                 waveOut.Stop();
-                
+
             }
             waveOut?.Dispose();
             mp3?.Dispose();
@@ -57,8 +64,8 @@ namespace CW
 
 
         public static PlaybackState Status()
-        {            
-            if (waveOut != null  )
+        {
+            if (waveOut != null)
             {
                 return waveOut.PlaybackState;
             }
